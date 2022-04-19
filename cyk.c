243b15518a1cd9ac894fb0d147da7a676a760d79
xie <601862884@qq.com>
2022-04-19 17:36:05 +0800

    untabify

--- cyk.c
+++ cyk.c
@@ -16,22 +16,22 @@
 #define MAX_STRING_LENGTH   1024 /* 待判断的字符串的最大长度 */
 
 struct binary_production {
-	int parent;
-	int left, right;
+    int parent;
+    int left, right;
 };
 struct unary_production {
-	int parent;
-	unsigned char ch;
+    int parent;
+    unsigned char ch;
 };
 struct sector {
-	int start;
-	int num;
+    int start;
+    int num;
 };
 typedef struct worker_load worker_load_t;
 struct worker_load {
-	int left_bound;			/* 左边界 */
-	int sub_len;			/* 子串长度 */
-	int sub_count;			/* 子任务数量 */
+    int left_bound;         /* 左边界 */
+    int sub_len;            /* 子串长度 */
+    int sub_count;          /* 子任务数量 */
 } worker_loads[4];
 
 int vn_number;
@@ -57,176 +57,176 @@ pthread_barrier_t barrier;
 int nsize = 3;
 
 int main() {
-	input();
-	initialize_table();
-	algo_main_body();
-	printf("%u\n", table_num[0][s_len - 1][0]);
-	return 0;
+    input();
+    initialize_table();
+    algo_main_body();
+    printf("%u\n", table_num[0][s_len - 1][0]);
+    return 0;
 }
 
 void input(void ) {
-	freopen("input.txt", "r", stdin);
-	scanf("%d\n", &vn_number);
-	scanf("%d\n", &binary_production_number);
-	int i;
-	for (i = 0; i < binary_production_number; i++)
-		scanf("<%d>::=<%d><%d>\n",
-			  &binaries[i].parent,
-			  &binaries[i].left,
-			  &binaries[i].right);
-	scanf("%d\n", &unary_production_number);
-	for (i = 0; i < unary_production_number; i++)
-		scanf("<%d>::=%c\n", &unaries[i].parent, &unaries[i].ch);
-	scanf("%d\n", &s_len);
-	scanf("%s\n", s);
+    freopen("input.txt", "r", stdin);
+    scanf("%d\n", &vn_number);
+    scanf("%d\n", &binary_production_number);
+    int i;
+    for (i = 0; i < binary_production_number; i++)
+        scanf("<%d>::=<%d><%d>\n",
+              &binaries[i].parent,
+              &binaries[i].left,
+              &binaries[i].right);
+    scanf("%d\n", &unary_production_number);
+    for (i = 0; i < unary_production_number; i++)
+        scanf("<%d>::=%c\n", &unaries[i].parent, &unaries[i].ch);
+    scanf("%d\n", &s_len);
+    scanf("%s\n", s);
 }
 
 int cmp_binary(const void *_a, const void *_b) {
-	const struct binary_production *a =
-			(const struct binary_production *)_a;
-	const struct binary_production *b =
-			(const struct binary_production *)_b;
-	return a->left == b->left ?
-							  (a->right == b->right ? a->parent -
-															  b->parent :
-													a->right - b->right)
-							  : a->left - b->left;
+    const struct binary_production *a =
+            (const struct binary_production *)_a;
+    const struct binary_production *b =
+            (const struct binary_production *)_b;
+    return a->left == b->left ?
+                              (a->right == b->right ? a->parent -
+                                                              b->parent :
+                                                    a->right - b->right)
+                              : a->left - b->left;
 }
 int cmp_unary(const void *_a, const void *_b) {
-	const struct unary_production *a =
-			(const struct unary_production *)_a;
-	const struct unary_production *b =
-			(const struct unary_production *)_b;
-	return a->ch == b->ch ? (a->parent - b->parent) :
-						  (a->ch < b->ch ? -1 : 1);
+    const struct unary_production *a =
+            (const struct unary_production *)_a;
+    const struct unary_production *b =
+            (const struct unary_production *)_b;
+    return a->ch == b->ch ? (a->parent - b->parent) :
+                          (a->ch < b->ch ? -1 : 1);
 }
 void initialize_table(void ) {
-	/* 初始化索引表 */
-	qsort(unaries, unary_production_number,
-		  sizeof(struct unary_production),
-		  cmp_unary);
-	qsort(binaries, binary_production_number,
-		  sizeof(struct binary_production),
-		  cmp_binary);
-	int i, j;
-	int left, right;
-	unsigned char ch;
-	int start, end;
-	int parent;
-	for (i = 0; i < MAX_VT_NUM; ++i) {
-		vt_index[i].start = -1;
-		vt_index[i].num = 0;
-	}
-	for (i = 0; i < unary_production_number; ++i) {
-		ch = unaries[i].ch;
-		if (vt_index[ch].start == -1) {
-			vt_index[ch].start = i;
-		}
-		vt_index[ch].num += 1;
-	}
-	for (i = 0; i < vn_number; ++i) {
-		for (j = 0; j < vn_number; ++j) {
-			vn_index[i][j].start = -1;
-			vn_index[i][j].num = 0;
-		}
-	}
-	for (i = 0; i < binary_production_number; ++i) {
-		left = binaries[i].left;
-		right = binaries[i].right;
-		if (vn_index[left][right].start == -1) {
-			vn_index[left][right].start = i;
-		}
-		vn_index[left][right].num += 1;
-	}
-	/* 填表的第一个斜对角线 */
-	for (i = 0; i < s_len; ++i) {
-		ch = s[i];
-		start = vt_index[ch].start;
-		end = vt_index[ch].start + vt_index[ch].num;
-		for (j = start; j < end; ++j) {
-			parent = unaries[j].parent;
-			if (!table_num[i][i][parent]) {
-				table_list[i][i][0]++;
-				table_list[i][i][table_list[i][i][0]] = parent;
-			}
-			table_num[i][i][parent]++;
-		}
-	}
+    /* 初始化索引表 */
+    qsort(unaries, unary_production_number,
+          sizeof(struct unary_production),
+          cmp_unary);
+    qsort(binaries, binary_production_number,
+          sizeof(struct binary_production),
+          cmp_binary);
+    int i, j;
+    int left, right;
+    unsigned char ch;
+    int start, end;
+    int parent;
+    for (i = 0; i < MAX_VT_NUM; ++i) {
+        vt_index[i].start = -1;
+        vt_index[i].num = 0;
+    }
+    for (i = 0; i < unary_production_number; ++i) {
+        ch = unaries[i].ch;
+        if (vt_index[ch].start == -1) {
+            vt_index[ch].start = i;
+        }
+        vt_index[ch].num += 1;
+    }
+    for (i = 0; i < vn_number; ++i) {
+        for (j = 0; j < vn_number; ++j) {
+            vn_index[i][j].start = -1;
+            vn_index[i][j].num = 0;
+        }
+    }
+    for (i = 0; i < binary_production_number; ++i) {
+        left = binaries[i].left;
+        right = binaries[i].right;
+        if (vn_index[left][right].start == -1) {
+            vn_index[left][right].start = i;
+        }
+        vn_index[left][right].num += 1;
+    }
+    /* 填表的第一个斜对角线 */
+    for (i = 0; i < s_len; ++i) {
+        ch = s[i];
+        start = vt_index[ch].start;
+        end = vt_index[ch].start + vt_index[ch].num;
+        for (j = start; j < end; ++j) {
+            parent = unaries[j].parent;
+            if (!table_num[i][i][parent]) {
+                table_list[i][i][0]++;
+                table_list[i][i][table_list[i][i][0]] = parent;
+            }
+            table_num[i][i][parent]++;
+        }
+    }
 }
 
 void sub_str_process(int i, int j) {
-	int k;
-	int p, q;
-	int p_range, q_range;
-	int B, C, A;
-	unsigned B_num, C_num;
-	int left, right;
-	int binary_index;
-	for (k = i; k <= j - 1; ++k) {
-		p_range = table_list[i][k][0];
-		q_range = table_list[k + 1][j][0];
-		for (p = 1; p <= p_range; ++p) {
-			for (q = 1; q <= q_range; ++q) {
-				B = table_list[i][k][p];
-				C = table_list[k + 1][j][q];
-				B_num = table_num[i][k][B];
-				C_num = table_num[k + 1][j][C];
-				left = vn_index[B][C].start;
-				right = left + vn_index[B][C].num;
-				for (binary_index = left;
-					 binary_index < right;
-					 ++binary_index) {
-					A = binaries[binary_index].parent;
-					table_num[i][j][A] ? 0 : (table_list[i][j][++table_list[i][j][0]] = A);
-					// if (!table_num[i][j][A]) {
-					// 	table_list[i][j][0]++;
-					// 	table_list[i][j][table_list[i][j][0]] = A;
-					// }
-					table_num[i][j][A] += B_num * C_num;
-				}
-			}
-		}
-	}
+    int k;
+    int p, q;
+    int p_range, q_range;
+    int B, C, A;
+    unsigned B_num, C_num;
+    int left, right;
+    int binary_index;
+    for (k = i; k <= j - 1; ++k) {
+        p_range = table_list[i][k][0];
+        q_range = table_list[k + 1][j][0];
+        for (p = 1; p <= p_range; ++p) {
+            for (q = 1; q <= q_range; ++q) {
+                B = table_list[i][k][p];
+                C = table_list[k + 1][j][q];
+                B_num = table_num[i][k][B];
+                C_num = table_num[k + 1][j][C];
+                left = vn_index[B][C].start;
+                right = left + vn_index[B][C].num;
+                for (binary_index = left;
+                     binary_index < right;
+                     ++binary_index) {
+                    A = binaries[binary_index].parent;
+                    table_num[i][j][A] ? 0 : (table_list[i][j][++table_list[i][j][0]] = A);
+                    // if (!table_num[i][j][A]) {
+                    //  table_list[i][j][0]++;
+                    //  table_list[i][j][table_list[i][j][0]] = A;
+                    // }
+                    table_num[i][j][A] += B_num * C_num;
+                }
+            }
+        }
+    }
 }
 
 void work(void *aux) {
-	struct worker_load *worker_load = (struct worker_load *)aux;
-	int i, j, sub_len = worker_load->sub_len;
-	for (i = worker_load->left_bound;
-		 i < worker_load->left_bound + worker_load->sub_count;
-		 ++i) {
-		j = i + sub_len - 1;
-		sub_str_process(i, j);
-	}
-	pthread_barrier_wait(&barrier);
+    struct worker_load *worker_load = (struct worker_load *)aux;
+    int i, j, sub_len = worker_load->sub_len;
+    for (i = worker_load->left_bound;
+         i < worker_load->left_bound + worker_load->sub_count;
+         ++i) {
+        j = i + sub_len - 1;
+        sub_str_process(i, j);
+    }
+    pthread_barrier_wait(&barrier);
 }
 
 void algo_main_body(void ) {
-	int i;
-	int sub_len;
-	for (sub_len = 2; sub_len <= s_len; ++sub_len) {
+    int i;
+    int sub_len;
+    for (sub_len = 2; sub_len <= s_len; ++sub_len) {
 #pragma omp parallel for
-		for (i = 0; i <= s_len - sub_len; ++i) {
-			int j = i + sub_len - 1;
-			sub_str_process(i, j);
-		}
-	}
+        for (i = 0; i <= s_len - sub_len; ++i) {
+            int j = i + sub_len - 1;
+            sub_str_process(i, j);
+        }
+    }
 }
 
 void split_work(worker_load_t loader[], int nsize, int sub_len) {
-	int all_count = s_len - sub_len + 1;
-	int single_count = all_count / nsize;
-	int i;
-	loader[0].left_bound = 0;
-	loader[0].sub_len = sub_len;
-	loader[0].sub_count = single_count;
-	all_count -= single_count;
-	for (i = 1; i < nsize; ++i) {
-		loader[i].left_bound = loader[i - 1].left_bound
-							   + loader[i - 1].sub_count;
-		loader[i].sub_len = sub_len;
-		loader[i].sub_count = (i != nsize - 1) ? single_count : all_count;
-		all_count -= single_count;
-	}
+    int all_count = s_len - sub_len + 1;
+    int single_count = all_count / nsize;
+    int i;
+    loader[0].left_bound = 0;
+    loader[0].sub_len = sub_len;
+    loader[0].sub_count = single_count;
+    all_count -= single_count;
+    for (i = 1; i < nsize; ++i) {
+        loader[i].left_bound = loader[i - 1].left_bound
+                               + loader[i - 1].sub_count;
+        loader[i].sub_len = sub_len;
+        loader[i].sub_count = (i != nsize - 1) ? single_count : all_count;
+        all_count -= single_count;
+    }
 }
 #pragma clang diagnostic pop
